9786d0eabbfe543bbe0fc42b73499be32185d904
SelinEogeniy <seljax92@gmail.com>
2025-07-02 16:54:20 +0300

    -make-task

---
+++ js/lecture-task.js
@@ -0,0 +1,79 @@
+/*
+ TODO: Виведи на екран загальну кількість яблук та винограду. Різницю яблук та винограду.
+ */
+
+// const apples = 135;
+// const grapes = 47;
+
+/*
+ TODO: Заміни вираз перевизначення комбінованим оператором.
+ */
+//~ Без комбінованого оператора
+// let students = 100;
+
+// students = students + 50;
+
+// console.log(students);
+
+/*
+  TODO: Дано 90 хвилин, дізнайтеся скільки в цьому числі міститься годин і хвилин.
+  TODO: Результат виведіть у консоль.
+*/
+
+// const total = 200;
+// const hour = Math.floor(total / 60);
+// const min = total % 60;
+
+// console.log(`${hour} hour : ${min} min`);
+
+/*
+  TODO: Запросіть у користувача ім'я та прізвище і виведіть у консоль повідомлення:
+  TODO: Hello <імʼя> <прізвище>, you are logged in.
+*/
+
+// const firstName = prompt("Введи им'я");
+// const lastName = prompt("Введи фамілію");
+
+// alert(`Hello ${firstName} ${lastName}, you are logged in.`);
+
+// TODO: Дізнайтесь довжину рядка message.
+
+// const message = prompt(
+//   "Введи любу кількість слов чи букв, я порахую скільки символів ти ввів"
+// );
+// alert(`Ви ввели ${message.length} символів`);
+
+/*
+ TODO: Склади фразу за допомогою шаблонних рядків
+ TODO: '{{companyName}} has {{repairBots + defenceBots}} bots in stock',
+ TODO: де companyName, repairBots, defenceBots - змінні вставлені в рядок.
+ */
+
+// const companyName = "Cyberdyne Systems";
+// const repairBots = 150;
+// const defenceBots = 50;
+
+// console.log(`${companyName} has ${repairBots + defenceBots} bots in stock`);
+
+/*
+TODO: Запросіть у користувача число, якщо число дорівнює 10,
+TODO: то виведіть 'Вірно', інакше виведіть 'Невірно'.
+*/
+
+// const msg = prompt("Введи число більше 10");
+// if (msg > 10) {
+//   alert("Вірно");
+// } else {
+//   alert("Не вірно");
+// }
+
+/*
+TODO: Перевірте чи є гроші на балансі, якщо є то виведіть до консолі 'Positive', інакше 'Negative'.
+*/
+
+// const balance = prompt("Введіть ваш баланс");
+// if (balance > 0) {
+//   alert("Positive");
+// } else {
+//   alert("Negative");
+// }
